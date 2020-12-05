38a067ba4d010ae0bd4d4cf23b1cff813e518f80
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-12-04 18:53:18 -0800

    whitespace

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
 const PORT = process.env.PORT || 3001;
 
