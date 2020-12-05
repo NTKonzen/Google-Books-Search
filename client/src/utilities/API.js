c1e92ac99f9c182d06ccfc790a5ea8651f9d8953
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-12-04 19:09:00 -0800

    Hid API Key

--- client/src/utilities/API.js
+++ client/src/utilities/API.js
@@ -10,7 +10,8 @@ const APICalls = {
     },
 
     getBooks(bookTitle) {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=AIzaSyDmTLWLcOBuZuitPxUEWhkdI3ivf6yzwfQ`)
+        const APIKey = process.env.REACT_APP_API_KEY || process.env.API_KEY
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${APIKey}`)
     }
 
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose')
 const path = require('path');
 const routes = require('./routes')
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
